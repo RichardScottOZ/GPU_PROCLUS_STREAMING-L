49349810b2f5ff85f264f1684859939052b0557a
RichardScottOZ <Richard.scott@bhp.com>
2025-11-11 14:19:10 +1030

    map modules

---
+++ src/map/GPU_PROCLUS_map.cpp
@@ -0,0 +1,28 @@
+#include <ATen/ATen.h>
+#include <torch/extension.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
+
+#include "../utils/util.h"
+#include "../utils/gpu_util.cuh"
+#include "../algorithms/PROCLUS.h"
+#include "../algorithms/GPU_PROCLUS.cuh"
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m
+) {
+m.def("PROCLUS",    &PROCLUS,    "");
+m.def("PROCLUS_parallel",    &PROCLUS_parallel,    "");
+m.def("PROCLUS_KEEP",    &PROCLUS_KEEP,    "");
+m.def("PROCLUS_KEEP_parallel",    &PROCLUS_KEEP_parallel,    "");
+m.def("PROCLUS_SAVE",    &PROCLUS_SAVE,    "");
+m.def("PROCLUS_SAVE_parallel",    &PROCLUS_SAVE_parallel,    "");
+m.def("PROCLUS_PARAM",    &PROCLUS_PARAM,    "");
+m.def("PROCLUS_PARAM_parallel",    &PROCLUS_PARAM_parallel,    "");
+m.def("GPU_PROCLUS",    &GPU_PROCLUS,    "");
+m.def("GPU_PROCLUS_KEEP",    &GPU_PROCLUS_KEEP,    "");
+m.def("GPU_PROCLUS_SAVE",    &GPU_PROCLUS_SAVE,    "");
+m.def("GPU_PROCLUS_PARAM",    &GPU_PROCLUS_PARAM,    "");
+m.def("GPU_PROCLUS_PARAM_2",    &GPU_PROCLUS_PARAM_2,    "");
+m.def("GPU_PROCLUS_PARAM_3",    &GPU_PROCLUS_PARAM_3,    "");
+}
