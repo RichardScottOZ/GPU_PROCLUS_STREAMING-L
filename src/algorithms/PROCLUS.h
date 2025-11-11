c2c4b17d434379905c005ed0a5ee3fffc2278b92
RichardScottOZ <Richard.scott@bhp.com>
2025-11-11 14:21:18 +1030

    proclus c++ header

---
+++ src/algorithms/PROCLUS.h
@@ -0,0 +1,38 @@
+#ifndef PROCLUS_GPU_PROCLUS_H
+#define PROCLUS_GPU_PROCLUS_H
+
+#include <ATen/ATen.h>
+#include <torch/extension.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+std::vector <at::Tensor>
+PROCLUS(at::Tensor data, int k, int l, float a, float b, float min_deviation, int termination_rounds, bool debug);
+
+std::vector <at::Tensor>
+PROCLUS_parallel(at::Tensor data, int k, int l, float a, float b, float min_deviation, int termination_rounds,
+                 bool debug);
+
+std::vector <at::Tensor>
+PROCLUS_KEEP(at::Tensor data, int k, int l, float a, float b, float min_deviation, int termination_rounds, bool debug);
+
+std::vector <at::Tensor>
+PROCLUS_KEEP_parallel(at::Tensor data, int k, int l, float a, float b, float min_deviation, int termination_rounds,
+                      bool debug);
+
+std::vector <at::Tensor>
+PROCLUS_SAVE(at::Tensor data, int k, int l, float a, float b, float min_deviation, int termination_rounds, bool debug);
+
+std::vector <at::Tensor>
+PROCLUS_SAVE_parallel(at::Tensor data, int k, int l, float a, float b, float min_deviation, int termination_rounds,
+                      bool debug);
+
+std::vector <std::vector<at::Tensor>>
+PROCLUS_PARAM(at::Tensor data, std::vector<int> ks, std::vector<int> ls, float a, float b, float min_deviation,
+              int termination_rounds, bool debug);
+
+std::vector <std::vector<at::Tensor>>
+PROCLUS_PARAM_parallel(at::Tensor data, std::vector<int> ks, std::vector<int> ls, float a, float b, float min_deviation,
+                       int termination_rounds, bool debug);
+
+#endif //PROCLUS_GPU_PROCLUS_H
